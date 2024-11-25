1e74133b33e8190b5ed1fe394d2b44a5a1a65bb5
Dundi vignesh Gutti <dundivignesh@Mac-183.lan>
2024-11-25 11:12:48 -0600

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana, Roboto } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"], weight: "500" });
+export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
